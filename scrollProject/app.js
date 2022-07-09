5815f9f9c8ff893e7ff5ca6e2636c7ffb702704b
Kishor Sarkar <kishor.sarkar@inadev.com>
2022-07-09 11:36:56 +0530

    Scroll project

---
+++ scrollProject/app.js
@@ -0,0 +1,9 @@
+const date = document.getElementById("date");
+date.innerHTML = new Date().getFullYear();
+
+
+const navToggle = document.querySelector(".nav-toggle");
+const linksContainer = document.querySelector(".links-container");
+const links = document.querySelector(".links");
+const navbar = document.getElementById("nav");
+const topLink = document.querySelector(".top-link");
